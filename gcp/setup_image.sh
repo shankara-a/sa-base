e8920779e7e2a602016ae3cb993f0d6e51188d4c
shankara-a <sanand@broadinstitute.org>
2020-02-28 18:41:22 +0000

    bashrc fix

--- gcp/setup_image.sh
+++ gcp/setup_image.sh
@@ -157,24 +157,3 @@ sudo chmod 700 /swapfile
 sudo mkswap /swapfile
 sudo swapon /swapfile
 echo "/swapfile none swap sw 0 0" | sudo tee -a /etc/fstab
-
-# -----------------------------
-# Helpers
-# -----------------------------
-echo "
-rmate ()
-{
-    export RMATE_PORT=$(python3 -c 'import port_for;print(port_for.select_random())');
-    echo 'Enter the following into the SSH menu:';
-    echo '-R $RMATE_PORT:localhost:52779';
-    unset -f rmate
-}
-
-jp ()
-{
-    export JUPYTER_PORT=$(python3 -c 'import port_for;print(port_for.select_random())');
-    echo 'Enter the following command in the ~C ssh menu:';
-    echo '-L $JUPYTER_PORT:localhost:$JUPYTER_PORT';
-    jupyter notebook --no-browser --ip=127.0.0.1 --port=$JUPYTER_PORT
-}
-" >> ~/.bashrc
